61af08daf21c6f200d20fb128ddf351c1d6de666
Ahmed Masood <ahmedandmehraj@icloud.com>
2022-04-05 11:34:26 +0100

    EERS

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@ variable "ami_id" {
   type = map(string)
 
   default = {
-    eu-west-2    = "ami-0f98541a3c898423d"
+    eu-west-2    = "ami-09a2a0f7d2db8baca"
     eu-central-1 = "ami-9787h5h6nsn"
   }
 }
